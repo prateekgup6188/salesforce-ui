b949caffd53c151a3f24b06082887bfa43ac710c
prateekgup6188 <prateekgupta6188@gmail.com>
2025-03-16 23:41:24 +0530

    changed host url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://salesforce-service.onrender.com';
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
